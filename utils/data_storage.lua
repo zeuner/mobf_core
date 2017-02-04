faf358393f1e8a21d8459b614e812ccc4e662f15
sapier <sapier at gmx dot net>
2017-02-04 18:05:41 +0100

    utils: data_storage, save data in a more human read and modifyable way

--- utils/data_storage.lua
+++ utils/data_storage.lua
@@ -63,7 +63,80 @@ utils.write_world_data = function(identifier, data)
 	end
 	mobf_assert_backtrace(file ~= nil)
 
-	local serialized_data = core.serialize(data)
+	local serialized_raw_data = core.serialize(data)
+	
+	local serialized_data = ""
+	local pos = 1
+	local depth = 0
+	local chars_since_open = 0
+	local non_whitespace_since_open = false
+	
+	for i = 1, serialized_raw_data:len(), 1 do
+		local last_char = serialized_raw_data:sub(i-1,i-1)
+		local char = serialized_raw_data:sub(i,i)
+		local next_char = serialized_raw_data:sub(i+1,i+1)
+
+		if char == "{" then
+
+			if next_char ~= "}" and last_char ~= "{" then
+				serialized_data = serialized_data .. "\n"
+
+				for j = 1, depth , 1 do
+					serialized_data = serialized_data .. "  "
+				end
+			
+			end
+				
+			serialized_data = serialized_data .. char
+			depth = depth + 1
+			
+			if depth < 0 then
+				depth = 0
+			end
+			
+			if next_char ~= "}" then
+				serialized_data = serialized_data .. "\n"
+				for j = 1, depth , 1 do
+					serialized_data = serialized_data .. "  "
+				end
+			end
+			
+			chars_since_open = 0
+			non_whitespace_since_open = false
+		elseif char == "}" then
+			depth = depth - 1
+			
+			if chars_since_open ~= 0 then
+			
+				if chars_since_open > 0 then
+					serialized_data = serialized_data .. "\n"
+				end
+
+				for j = 1, depth , 1 do
+					serialized_data = serialized_data .. "  "
+				end
+			end
+			serialized_data = serialized_data .. char
+			
+			if next_char ~= "," then
+				serialized_data = serialized_data .. "\n"
+			end
+			chars_since_open = -1
+		elseif char == "," and last_char == "}" then
+			serialized_data = serialized_data .. char
+			serialized_data = serialized_data .. "\n"
+			for j = 1, depth , 1 do
+				serialized_data = serialized_data .. "  "
+			end
+		elseif  char ~= nil then
+		
+			if char ~= " " or non_whitespace_since_open then
+				serialized_data = serialized_data .. char
+				chars_since_open = chars_since_open + 1
+				non_whitespace_since_open = true
+			end
+		end
+	end
 
 	file:write(serialized_data)
 	file:close()
